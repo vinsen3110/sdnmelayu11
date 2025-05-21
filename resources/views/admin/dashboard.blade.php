0ff1c899780b4ed629344da72630a61f01f8d749
Alifa K.R <alifaa19>
2025-05-21 22:32:19 +0800

    push admin

--- resources/views/admin/dashboard.blade.php
+++ resources/views/admin/dashboard.blade.php
@@ -8,7 +8,7 @@
             >
               <div>
                 <h3 class="fw-bold mb-3">Dashboard</h3>
-                <h6 class="op-7 mb-2">Free Bootstrap 5 Admin Dashboard</h6>
+                <h6 class="op-7 mb-2">SD Negeri Melayu 11 Banjarmasin</h6>
               </div>
               <div class="ms-md-auto py-2 py-md-0">
                 <a href="#" class="btn btn-label-info btn-round me-2">Manage</a>
--- resources/views/layouts/admin.blade.php
+++ resources/views/layouts/admin.blade.php
@@ -2,7 +2,7 @@
 <html lang="en">
   <head>
     <meta http-equiv="X-UA-Compatible" content="IE=edge" />
-    <title>Kaiadmin - Bootstrap 5 Admin Dashboard</title>
+    <title>SDN Melayu 11 BJM - Admin Dashboard</title>
     <meta
       content="width=device-width, initial-scale=1.0, shrink-to-fit=no"
       name="viewport"
@@ -47,27 +47,30 @@
       <div class="sidebar" data-background-color="dark">
         <div class="sidebar-logo">
           <!-- Logo Header -->
-          <div class="logo-header" data-background-color="dark">
-            <a href="index.html" class="logo">
-              <img
-                src="{{asset('assets/img/kaiadmin/logo_light.svg')}}"
-                alt="navbar brand"
-                class="navbar-brand"
-                height="20"
-              />
-            </a>
-            <div class="nav-toggle">
-              <button class="btn btn-toggle toggle-sidebar">
-                <i class="gg-menu-right"></i>
-              </button>
-              <button class="btn btn-toggle sidenav-toggler">
-                <i class="gg-menu-left"></i>
-              </button>
-            </div>
-            <button class="topbar-toggler more">
-              <i class="gg-more-vertical-alt"></i>
-            </button>
-          </div>
+          <div class="logo-header d-flex align-items-center" data-background-color="dark">
+    <a href="index.html" class="logo d-flex align-items-center">
+        <img
+            src="{{ asset('assets/img/logo_sekolah.png') }}"
+            alt="navbar brand"
+            class="navbar-brand"
+            height="35"
+        />
+        <span class="ms-2 text-white fw-bold" style="font-size: 12px;">SDN MELAYU 11 BJM</span>
+ <!-- ← Tambah teks di sini -->
+    </a>
+    <div class="nav-toggle ms-auto">
+        <button class="btn btn-toggle toggle-sidebar">
+            <i class="gg-menu-right"></i>
+        </button>
+        <button class="btn btn-toggle sidenav-toggler">
+            <i class="gg-menu-left"></i>
+        </button>
+    </div>
+    <button class="topbar-toggler more">
+        <i class="gg-more-vertical-alt"></i>
+    </button>
+</div>
+
           <!-- End Logo Header -->
         </div>
         <div class="sidebar-wrapper scrollbar scrollbar-inner">
