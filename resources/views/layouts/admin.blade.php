6411b6287f982591c86d96f289ae1e2b1b330968
maisyamaulidia <andi.maisya22@gmail.com>
2025-07-08 07:56:37 +0800

    admin

--- resources/views/layouts/admin.blade.php
+++ resources/views/layouts/admin.blade.php
@@ -10,8 +10,8 @@
     />
     <link
       rel="icon"
-      href="{{asset('admin/assets/img/kaiadmin/favicon.ico')}}"
-      type="image/x-icon"
+      href="{{ asset('ta/img/logo.png') }}"
+      type="image/png"
     />
 
     <!-- Fonts and icons -->
@@ -35,13 +35,13 @@
     </script>
 
     <!-- CSS Files -->
-    <link rel="stylesheet" href="{{asset('assets/css/bootstrap.min.css')}}" />
-    <link rel="stylesheet" href="{{asset('assets/css/plugins.min.css')}}" />
-    <link rel="stylesheet" href="{{asset('assets/css/kaiadmin.min.css')}}" />
+    <link rel="stylesheet" href="{{asset('admin/assets/css/bootstrap.min.css')}}" />
+    <link rel="stylesheet" href="{{asset('admin/assets/css/plugins.min.css')}}" />
+    <link rel="stylesheet" href="{{asset('admin/assets/css/kaiadmin.min.css')}}" />
   </head>
   <head>
     <!-- CSS Just for demo purpose, don't include it in your project -->
-    <link rel="stylesheet" href="{{asset('assets/css/demo.css')}}" />
+    <link rel="stylesheet" href="{{asset('admin/assets/css/demo.css')}}" />
   </head>
   <body>
     <div class="wrapper">
@@ -51,7 +51,7 @@
           <!-- Logo Header -->
           <div class="logo-header d-flex align-items-center" data-background-color="dark">
              <img
-            src="{{ asset('assets/img/logo_sekolah.png') }}"
+            src="{{ asset('admin/assets/img/logo_sekolah.png') }}"
             alt="navbar brand"
             class="navbar-brand"
             height="35"
@@ -384,41 +384,41 @@
       <!-- End Custom template -->
     </div>
     <!--   Core JS Files   -->
-    <script src="{{asset('assets/js/core/jquery-3.7.1.min.js')}}"></script>
-    <script src="{{asset('assets/js/core/popper.min.js')}}"></script>
-    <script src="{{asset('assets/js/core/bootstrap.min.js')}}"></script>
+    <script src="{{asset('admin/assets/js/core/jquery-3.7.1.min.js')}}"></script>
+    <script src="{{asset('admin/assets/js/core/popper.min.js')}}"></script>
+    <script src="{{asset('admin/assets/js/core/bootstrap.min.js')}}"></script>
 
     <!-- jQuery Scrollbar -->
-    <script src="{{asset('assets/js/plugin/jquery-scrollbar/jquery.scrollbar.min.js')}}"></script>
+    <script src="{{asset('admin/assets/js/plugin/jquery-scrollbar/jquery.scrollbar.min.js')}}"></script>
 
     <!-- Chart JS -->
-    <script src="{{asset('assets/js/plugin/chart.js/chart.min.js')}}"></script>
+    <script src="{{asset('admin/assets/js/plugin/chart.js/chart.min.js')}}"></script>
 
     <!-- jQuery Sparkline -->
-    <script src="{{asset('assets/js/plugin/jquery.sparkline/jquery.sparkline.min.js')}}"></script>
+    <script src="{{asset('admiin/assets/js/plugin/jquery.sparkline/jquery.sparkline.min.js')}}"></script>
 
     <!-- Chart Circle -->
-    <script src="{{asset('assets/js/plugin/chart-circle/circles.min.js')}}"></script>
+    <script src="{{asset('admin/assets/js/plugin/chart-circle/circles.min.js')}}"></script>
 
     <!-- Datatables -->
-    <script src="{{asset('assets/js/plugin/datatables/datatables.min.js')}}"></script>
+    <script src="{{asset('admin/assets/js/plugin/datatables/datatables.min.js')}}"></script>
 
     <!-- Bootstrap Notify -->
-    <script src="{{asset('assets/js/plugin/bootstrap-notify/bootstrap-notify.min.js')}}"></script>
+    <script src="{{asset('admin/assets/js/plugin/bootstrap-notify/bootstrap-notify.min.js')}}"></script>
 
     <!-- jQuery Vector Maps -->
-    <script src="{{asset('assets/js/plugin/jsvectormap/jsvectormap.min.js')}}"></script>
-    <script src="{{asset('assets/js/plugin/jsvectormap/world.js')}}"></script>
+    <script src="{{asset('admin/assets/js/plugin/jsvectormap/jsvectormap.min.js')}}"></script>
+    <script src="{{asset('admin/assets/js/plugin/jsvectormap/world.js')}}"></script>
 
     <!-- Sweet Alert -->
-    <script src="{{asset('assets/js/plugin/sweetalert/sweetalert.min.js')}}"></script>
+    <script src="{{asset('admin/assets/js/plugin/sweetalert/sweetalert.min.js')}}"></script>
 
     <!-- Kaiadmin JS -->
-    <script src="{{asset('assets/js/kaiadmin.min.js')}}"></script>
+    <script src="{{asset('admin/assets/js/kaiadmin.min.js')}}"></script>
 
     <!-- Kaiadmin DEMO methods, don't include it in your project! -->
-    <script src="{{asset('assets/js/setting-demo.js')}}"></script>
-    <script src="{{asset('assets/js/demo.js')}}"></script>
+    <script src="{{asset('admin/assets/js/setting-demo.js')}}"></script>
+    <script src="{{asset('admin/assets/js/demo.js')}}"></script>
     <script>
       $("#lineChart").sparkline([102, 109, 120, 99, 110, 105, 115], {
         type: "line",
